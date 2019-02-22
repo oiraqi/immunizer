41c0336e80ea017af245952ea6d26887b08931aa
Omar Iraqi <o.iraqi@gmail.com>
2019-02-22 06:05:01 +0100

    Adding comments to OFBizInterceptAgent

--- framework/OFBizInterceptAgent.java
+++ framework/OFBizInterceptAgent.java
@@ -17,11 +17,14 @@ public class OFBizInterceptAgent {
 		new AgentBuilder.Default().ignore(nameStartsWith("net.bytebuddy."))
 				.type(named("org.apache.ofbiz.webapp.control.ControlFilter")
 						/**
+						 * //These are (re)activated for general scenarios and efficiency evaluation
 						 * nameStartsWith("org.apache.ofbiz.accounting.invoice.")
 						 * .or(nameStartsWith("org.apache.ofbiz.accounting.payment."))
 						 * .or(nameStartsWith("org.apache.ofbiz.accounting.util."))
 						 */
-						.or(named("org.apache.ofbiz.entity.datasource.GenericDAO")))
+						.or(named("org.apache.ofbiz.entity.datasource.GenericDAO"))) // enough for our effectiveness
+																						// evaluation scenario (the
+																						// invoice update form)
 				.transform(new InterceptTransformer()).installOn(inst);
 	}
 
@@ -30,6 +33,10 @@ public class OFBizInterceptAgent {
 		public DynamicType.Builder<?> transform(final DynamicType.Builder<?> builder,
 				final TypeDescription typeDescription, final ClassLoader classLoader, final JavaModule module) {
 
+			// .and(named("update")) is enough for our effectiveness evaluation scenario
+			// (the invoice update form)
+			// should keep just .method(isPublic()) for general scenarios and efficiency
+			// evaluation
 			return builder.method(isPublic().and(named("doFilter"))).intercept(Advice.to(ControllerMethodAdvice.class))
 					.method(isPublic().and(named("update"))).intercept(Advice.to(ModelViewMethodAdvice.class));
 		}
@@ -40,17 +47,18 @@ public class OFBizInterceptAgent {
 		public static FeatureExtractor featureExtractorSingleton = FeatureExtractor.getSingleton();
 
 		@Advice.OnMethodEnter
-		public static Invocation onEnter(/*@Advice.This Object object, */@Advice.Origin String fullyQualifiedMethodName,
+		public static Invocation onEnter(
+				/* @Advice.This Object object, */@Advice.Origin String fullyQualifiedMethodName,
 				@Advice.AllArguments Object[] params) {
 
 			String userAgent = null;
 			try {
-				userAgent = (String) params[0].getClass().getMethod("getHeader", java.lang.String.class).invoke(params[0],
-						"User-Agent");
+				userAgent = (String) params[0].getClass().getMethod("getHeader", java.lang.String.class)
+						.invoke(params[0], "User-Agent");
 				if (userAgent == null || !userAgent.equals("JMeter")) {
 					userAgent = "ZAP"; /**
-										 * Label it as true positive, just for automatic evaluation 
-										 * of intrusion/oulier detection results
+										 * Label it as true positive, just for automatic evaluation of intrusion/oulier
+										 * detection results
 										 */
 				}
 			} catch (Exception ex) {
@@ -102,26 +110,25 @@ public class OFBizInterceptAgent {
 			if (invocation.getFullyQualifiedMethodName().equals(
 					"public int org.apache.ofbiz.entity.datasource.GenericDAO.update(org.apache.ofbiz.entity.GenericEntity) throws org.apache.ofbiz.entity.GenericEntityException")) {
 				boolean relevant = false;
-				//System.out.println("XXXXXXXXXXXXXXXXXXXXXXXX");
+				// System.out.println("XXXXXXXXXXXXXXXXXXXXXXXX");
 				for (StackTraceElement ste : Thread.currentThread().getStackTrace()) {
-					//System.out.println(ste);
+					// System.out.println(ste);
 					if (ste.getClassName().equals("org.apache.ofbiz.minilang.SimpleMethod")) {
 						relevant = true;
 						break;
 					}
 				}
-				//System.out.println("XXXXXXXXXXXXXXXXXXXXXXXX");
+				// System.out.println("XXXXXXXXXXXXXXXXXXXXXXXX");
 				if (!relevant)
 					return;
 			}
 			if (invocation.returns()) {
-				if (thrown != null){
-					if(invocation.returnsNumber())
+				if (thrown != null) {
+					if (invocation.returnsNumber())
 						invocation.update(Integer.valueOf("0"), true);
 					else
-						invocation.update("NULL", true); 
-				}
-				else if (result == null)
+						invocation.update("NULL", true);
+				} else if (result == null)
 					invocation.update("NULL", false);
 				else
 					invocation.update(result, false);
