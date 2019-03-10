14278bca68730682a370b3e3b45d16206aa44e5f
Omar Iraqi <o.iraqi@gmail.com>
2019-03-10 11:02:57 +0100

    New results for new LOF.k values

--- evaluation/Evaluator.java
+++ evaluation/Evaluator.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class Evaluator{
     private static String __LABEL__ = " ZAP ";
@@ -20,12 +22,34 @@ public class Evaluator{
         BufferedWriter bw = new BufferedWriter(
                                     new OutputStreamWriter(new FileOutputStream(evaluationOutFolder + "/r_precision.txt")));
         File[] subFolders = new File(elkiOutFolder).listFiles();
+        Arrays.sort(subFolders);
         for(File subFolder : subFolders){
             File[] files = subFolder.listFiles();
+            Arrays.sort(files, new Comparator<File>() {
+                public int compare(File f1, File f2){
+                    if(f1 == null && f2 == null)
+                        return 0;
+                    if(f1 == null)
+                        return -1;
+                    if (f2 == null)
+                        return 1;
+                    if ((f1.getName().indexOf('_') > 0) && (f2.getName().indexOf('_') > 0)){
+                        String suffix1 = f1.getName().substring(f1.getName().indexOf('_') + 1, f1.getName().indexOf('.'));
+                        String suffix2 = f2.getName().substring(f2.getName().indexOf('_') + 1, f2.getName().indexOf('.'));
+                        try{
+                            return Integer.parseInt(suffix1) - Integer.parseInt(suffix2);
+                        }catch(Exception ex){
+                            return f1.compareTo(f2);
+                        }
+                    }else{
+                        return f1.compareTo(f2);
+                    }
+                }
+            });
             for(File file : files){
                 BufferedReader br = new BufferedReader(
                                     new InputStreamReader(new FileInputStream(file)));
-                bw.write(file.getName().substring(0, file.getName().indexOf('.')) + ": " + computeRPrecision(br, r) + "\t");
+                bw.write(file.getName().substring(0, file.getName().indexOf('.')) + ": " + computeRPrecision(br, r) + "\n");
                 br.close();
             }
             bw.write("\n\n");
