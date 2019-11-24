291ecbf2a77a8e6d695f2bb2e85bf7fc0c892d78
Omar Iraqi <o.iraqi@gmail.com>
2019-11-24 09:00:10 +0100

    fixed and enhanced ofbiz-immunizer.sh

--- scripts/ofbiz-immunized.sh
+++ scripts/ofbiz-immunized.sh
@@ -1,6 +1,5 @@
 cd ../framework
 ./gradlew agentLibs
-cd build/classes/java/main
-jar cfm ../../../libs/ofbiz-immunizer-agent.jar ../../../../../scripts/manifest-ofbiz.mf org/immunizer/apps/ofbiz/*.class
-cd ../../../../../../ofbiz-framework
+jar cfm ./build/libs/ofbiz-immunizer-agent.jar ../scripts/manifest-ofbiz.mf
+cd ../../ofbiz-framework
 java -javaagent:../immune-apps/framework/build/libs/ofbiz-immunizer-agent.jar -Dmodels=../immune-apps/models/ofbiz -Dbuffer=100 -Dskip=5000 -jar build/libs/ofbiz.jar
