f9b4232448ec4a441cbd70768f1b39095686384c
Omar Iraqi <o.iraqi@gmail.com>
2020-01-07 22:42:25 +0100

    deleted some folders

--- scripts/ofbiz-immunized.sh
+++
@@ -1,5 +0,0 @@
-cd ../framework
-./gradlew agentLibs
-jar cfm ./build/libs/ofbiz-immunizer-agent.jar ../scripts/manifest-ofbiz.mf
-cd ../../ofbiz-framework
-java -javaagent:../immune-apps/framework/build/libs/ofbiz-immunizer-agent.jar -Dmodels=../immune-apps/models/ofbiz -Dbuffer=100 -Dskip=5000 -jar build/libs/ofbiz.jar
