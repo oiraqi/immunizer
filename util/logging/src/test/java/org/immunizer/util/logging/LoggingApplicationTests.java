4c0fff8f47409aa1836d26e6d9ba635f30ed821b
Omar Iraqi <o.iraqi@gmail.com>
2021-12-24 23:41:04 +0100

    updates

---
+++ util/logging/src/test/java/org/immunizer/util/logging/LoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.immunizer.util.logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
